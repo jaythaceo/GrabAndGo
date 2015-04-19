2d5dbe45996610593f1e7671d43e0827f293644e
jaythaceo <jaythaceo@gmail.com>
2015-04-19 19:11:32 -0400

    [chore] Retrived player ID

--- parser/fetch_info.py
+++ parser/fetch_info.py
@@ -1,1 +1,12 @@
 # XML Parser
+import urllib
+from bs4 import BeautifulSoup
+
+URL = 'http://www.cbssports.com/mlb/stats/playersort/mlb/year-2015-season-regularseason-category-batting-qualifying-1'
+
+html = urllib.urlopen(URL).read()
+soup = BeautifulSoup(html)
+tags = soup('tr')
+
+for tag in tags:
+  tag.get('id', None)
