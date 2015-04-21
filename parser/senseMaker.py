68e8fc93af57e7faabdd8c3aa1a811cb1ed59eb9
jaythaceo <jaythaceo@gmail.com>
2015-04-21 14:10:53 -0400

    [chore] Added line_count function

--- parser/senseMaker.py
+++ parser/senseMaker.py
@@ -2,3 +2,9 @@
 # Read data from the boards.txt file
 
 boards_file = open('boards.txt', "r")
+
+def line_count():
+  count = 0
+  for line in boards_file:
+    count = count + 1
+  print "Line count: ", count
