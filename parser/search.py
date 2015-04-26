796fd483aca16c4bac3af8c27e757c56a3f84073
jaythaceo <jaythaceo@gmail.com>
2015-04-25 23:01:01 -0400

    [chore] Removed search.py added up_loader.py

--- parser/search.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import re
-
-files = open('boards.txt', 'r')
-
-for line in files:
-  if re.match("(.*)(E|e)lections(.*)", line):
-    print line
---
+++ parser/up_loader.py
@@ -0,0 +1,6 @@
+"""HTTPCONNECTION
+This file is used to proform a
+memory effcient upload to a server using
+a POST request something that is not easily
+accomplished using urllib
+"""
